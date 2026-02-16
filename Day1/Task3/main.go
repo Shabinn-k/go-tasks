c34c727b23bf22e7beb1ecae3d70b7cc09996a21
Shabinn-k <shhabin.07@gmail.com>
2026-02-16 17:06:09 +0530

    completed fibonacci

--- Day1/Task3/main.go
+++ Day1/Task3/main.go
@@ -1,5 +1,18 @@
+// Fibanocci series with a loop
 package main
 
+import "fmt"
+
 func main() {
-	
+	var num1 int
+	a := 0
+	b := 1
+	fmt.Println("Enter a number :")
+	fmt.Scan(&num1)
+	for i := 0; i <= num1; i++ {
+		fmt.Println(a)
+		next := a + b
+		a = b
+		b = next
+	}
 }
