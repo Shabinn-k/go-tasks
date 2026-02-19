ae03af052f32df077a4be267fa6eae925f0a4cc1
Shabinn-k <shhabin.07@gmail.com>
2026-02-19 16:28:54 +0530

    did task of finding max and min of an arrayt

--- Day3/Task1/main.go
+++ Day3/Task1/main.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
+func maxMin(arr [5]int) (int, int) {
+	max := arr[0]
+	min := arr[0]
+
+	for i := 1; i < len(arr); i++ {
+		if arr[i] > max {
+			max = arr[i]
+		}
+		if arr[i] < min {
+			min = arr[i]
+		}
+	}
+	return max, min
+
+}
 func main() {
-	
+	arr := [5]int{23,13,1,532,65}
+	max, min := maxMin(arr)
+	fmt.Println(max)
+	fmt.Println(min)
 }
