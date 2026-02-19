3a6b8ad867a29c66e7bb481a8d1dbcc64a11b31a
Shabinn-k <shhabin.07@gmail.com>
2026-02-19 16:59:53 +0530

    cleared task using map functionn

--- Day3/Task6/main.go
+++ Day3/Task6/main.go
@@ -1,5 +1,15 @@
 package main
-
+import "fmt"
+type Student struct{
+	Name string
+	Grade int
+	Age int
+}
 func main() {
-	
+	studentInfo:=map[string]Student{
+		"shabin":{Age:18,Grade:123,},
+		"john":{Age:11,Grade:100,},
+	}
+	fmt.Println(studentInfo["shabin"].Age)
+	fmt.Println(studentInfo["john"].Grade)
 }
