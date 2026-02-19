ce52bf45ce53a6a95e61d7762f4264724860c05f
Shabinn-k <shhabin.07@gmail.com>
2026-02-19 16:49:55 +0530

    did task of listing students stored in struct using for loop

--- Day3/Task5/main.go
+++ Day3/Task5/main.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
+type Student struct {
+	Name string
+	Age  int
+}
+
 func main() {
-	
+	students := []Student{
+		{Name: "shabin", Age: 18,},
+		{Name: "john", Age: 20,},
+	}
+	for _, value := range students {
+		fmt.Println(value.Name,"-",value.Age)
+	}
 }
