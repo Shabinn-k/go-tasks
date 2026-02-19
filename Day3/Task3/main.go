61e6733e05abc76d4945f7ec4bf0b04206d4cc09
Shabinn-k <shhabin.07@gmail.com>
2026-02-19 16:39:57 +0530

    did task of listing students using map and printing them

--- Day3/Task3/main.go
+++ Day3/Task3/main.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	students := make(map[string]int)
+	students["shabin"] = 81
+	students["john"] = 51
+	students["rahul"] = 11
+
+	fmt.Println("Shabin Grade",students["shabin"])
+
+	for name,grade:=range students{
+		fmt.Println(name,": ",grade)
+	}
 }
