a35d0127df70f33ad5ec1b90acdbf463b7cd2c3b
Shabinn-k <shhabin.07@gmail.com>
2026-02-19 16:44:20 +0530

    did task of listing students using struct function and disp[laying them

--- Day3/Task4/main.go
+++ Day3/Task4/main.go
@@ -1,5 +1,16 @@
 package main
+import "fmt"
+
+type Student struct{
+	Name string
+	Age int
+	Grade int
+}
 
 func main() {
-	
+	students:=[]Student{
+		{Name: "shabin",Age: 18,Grade: 62},
+		{Name: "john",Age: 20,Grade: 12},
+		}	
+		fmt.Println(students)
 }
